e24854ee736754c3c96801c2c2e58ff2d6723bab
Steeves Obiang Ndam <steevesobiang@gmail.com>
2015-11-09 17:15:10 +0000

    item selected

--- app/src/main/java/com/example/android/sunshine/app/ForcastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForcastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -74,7 +76,7 @@ public class ForcastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        /*
+
         String[] forecastArray ={
                 "Today - Sunny - 88/63",
                 "Tomorrow - Foggy - 70/40",
@@ -99,7 +101,15 @@ public class ForcastFragment extends Fragment {
                         weekForecast);
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-        */
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String forecast = mForecastAdapter.getItem(position);
+                Toast toast = Toast.makeText(getActivity(),forecast,Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
         return rootView;
     }
 
