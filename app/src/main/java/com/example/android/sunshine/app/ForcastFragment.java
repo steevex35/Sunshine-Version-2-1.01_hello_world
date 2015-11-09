ad805f6ce83bc62466adb43ecc3f8d0d43d97b18
Steeves Obiang Ndam <steevesobiang@gmail.com>
2015-11-09 18:00:04 +0000

    Activity Detal

--- app/src/main/java/com/example/android/sunshine/app/ForcastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForcastFragment.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -106,8 +107,11 @@ public class ForcastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 String forecast = mForecastAdapter.getItem(position);
-                Toast toast = Toast.makeText(getActivity(),forecast,Toast.LENGTH_SHORT);
-                toast.show();
+                //Toast toast = Toast.makeText(getActivity(),forecast,Toast.LENGTH_SHORT);
+                //toast.show();
+                Intent intent = new Intent(getActivity(),DetailActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT,forecast);
+                startActivity(intent);
             }
         });
         return rootView;
