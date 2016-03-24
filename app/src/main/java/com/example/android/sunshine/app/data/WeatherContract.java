65ff85e16333a3c1b9917aaa2ce802de6ed197be
Steeves Obiang Ndam <steevesobiang@gmail.com>
2016-03-24 21:23:43 +0100

    DATA and Contract

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -39,7 +39,16 @@ public class WeatherContract {
         done for WeatherEntry)
      */
     public static final class LocationEntry implements BaseColumns {
+
+
         public static final String TABLE_NAME = "location";
+
+        public  static final String COLUMN_LOCATION_SETTING="location_setting";
+
+        public  static final String COLUMN_CITY_NAME="city_name";
+
+        public  static final String COLUMN_COORD_LAT="coord_lat";
+        public  static final String  COLUMN_COORD_LONG="coord_long";
 
     }
 
